007c2d3d222358ecfd40dbade06f05caa828281f
danielkarling3 <danielantoniokarling3@gmail.com>
2018-09-11 17:22:03 -0300

    Update events.js

--- events.js
+++ events.js
@@ -13,4 +13,26 @@ AFRAME.registerComponent('markerhandler', {
                 entity.setAttribute('scale', scale);
             }
         });
-}});
+    }
+    init: function () {
+        this.playVideo = this.playVideo.bind(this);
+        this.playVideoNextTick = this.playVideoNextTick.bind(this);
+      },
+      play: function () {
+        window.addEventListener('vrdisplayactivate', this.playVideo);
+        this.el.sceneEl.addEventListener('enter-vr', this.playVideoNextTick);
+      },
+      pause: function () {
+        this.el.sceneEl.removeEventListener('enter-vr', this.playVideoNextTick);
+        window.removeEventListener('vrdisplayactivate', this.playVideo);
+      },
+      playVideoNextTick: function () {
+        setTimeout(this.playVideo);
+      },
+      playVideo: function () {
+        var video = this.el.components.material.material.map.image;
+        if (!video) { return; }
+        video.play();
+  }
+});
+
